8e2ea2a736696e2f56b593545203564f5a83e703
Dane Jackson <39383664+danedjackson@users.noreply.github.com>
2021-03-25 20:42:34 +0500

    Update server_pop.js
    Allowing 0 count

--- src/server_pop.js
+++ src/server_pop.js
@@ -1,12 +1,10 @@
 module.exports = async function (client, count) {
-    const guildID = process.env.GUILDID;
 
     async function updateCount () {
-        if(count){
+        if(count >=0 ){
             client.user.setActivity(`${count} / 150`, { type: 'WATCHING' });
         }
     }
-
-    const guild = client.guilds.cache.get(guildID);
+    
     return await updateCount();
 }
