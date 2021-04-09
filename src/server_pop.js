2a2256c5505b94c0589f6658d3aede45d99bfdc1
Dane Jackson <39383664+danedjackson@users.noreply.github.com>
2021-04-09 16:45:54 -0500

    Update server_pop.js

--- src/server_pop.js
+++ src/server_pop.js
@@ -1,9 +1,13 @@
 module.exports = async function (client, count) {
 
     async function updateCount () {
+         if(!count) {
+             client.user.setActivity(`0/ 150`, { type: 'WATCHING' });
+         }
          if(count >=0 ){
             client.user.setActivity(`${count} / 150`, { type: 'WATCHING' });
         }
+        
     }
     
     return await updateCount();
